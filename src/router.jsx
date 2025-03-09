a392266ed61d01ab2fe05b9275d7fed961c94f9e
keshavulu1 <keshavulumuppa03@gmail.com>
2025-03-09 23:49:25 +0530

    Initial commit

---
+++ src/router.jsx
@@ -0,0 +1,19 @@
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+  {
+    path: "country",
+    element: <Country />,
+  },
+  {},
+]);
